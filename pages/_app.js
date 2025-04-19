4c65bdbde5a5b7916e1cc06470a6376d8bd9b270
Robert McElhinney <robmcelhinney@hotmail.com>
2025-04-19 11:30:29 +0100

    add favicon

--- pages/_app.js
+++ pages/_app.js
@@ -1,10 +1,32 @@
 import "../styles/globals.css"
 import Header from "../components/Header"
 import { ThemeProvider } from "../components/ThemeContext"
+import Head from "next/head"
 
 function MyApp({ Component, pageProps }) {
     return (
         <ThemeProvider>
+            <Head>
+                <link rel="icon" href="/favicon.ico" />
+                <link
+                    rel="apple-touch-icon"
+                    sizes="180x180"
+                    href="/apple-touch-icon.png"
+                />
+                <link
+                    rel="icon"
+                    type="image/png"
+                    sizes="32x32"
+                    href="/favicon-32x32.png"
+                />
+                <link
+                    rel="icon"
+                    type="image/png"
+                    sizes="16x16"
+                    href="/favicon-16x16.png"
+                />
+                <link rel="manifest" href="/site.webmanifest" />
+            </Head>
             <Header />
             <Component {...pageProps} />
         </ThemeProvider>
